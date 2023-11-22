e8e7a1eddaceaff0fb072ea6f5cfc9596e11388f
Jem Suarez <jempoolsmz@gmail.com>
2023-11-22 17:00:58 -0500

    Adding Signup API

--- api/authentication/controllers/authentication.js
+++ api/authentication/controllers/authentication.js
@@ -23,4 +23,39 @@ module.exports = {
       return ctx.send({ message: 'Incorrect email or password.' }, 400);
     }
   },
+
+  async signup(ctx) {
+    try {
+      const { name, email, password } = ctx.request.body;
+
+      // Check if there is already a user associated with this email
+      const _user = await strapi.plugins['users-permissions'].services.user.fetch({ email });
+      if (_user) {
+        return ctx.send({ message: `The email ${email} is already associated with an account` }, 400);
+      }
+
+      // Fetch the role
+      const role = await strapi.query('role', 'users-permissions').findOne({ name: 'Authenticated' });
+
+      const payload = {
+        username: name,
+        email,
+        password,
+        confirmed: true,
+        provider: 'local',
+        role: role.id
+      };
+
+      const newUser = await strapi.plugins['users-permissions'].services.user.add(payload);
+
+      const user = { name, email }
+      const accessToken = strapi.plugins['users-permissions'].services.jwt.issue({ id: newUser.id, user });
+      const refreshToken = jwt.sign({ id: newUser.id, user }, process.env.JWT_SECRET, { expiresIn: '1d' });
+
+      return ctx.send({ user, accessToken, refreshToken });
+    } catch (error) {
+      console.log(error);
+      return ctx.send({ message: 'Incorrect email or password.' }, 400);
+    }
+  }
 };
