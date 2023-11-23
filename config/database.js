f1e3fed2a8fdcea35e46d9b2d4b8d54dcc6bc247
Jem Suarez <jempoolsmz@gmail.com>
2023-11-22 20:28:22 -0500

    Fixing format

--- config/database.js
+++ config/database.js
@@ -5,16 +5,9 @@ module.exports = ({ env }) => ({
       connector: 'mongoose',
       settings: {
         host: '0.0.0.0',
-        // srv: env.bool('DATABASE_SRV', false),
         port: 27017,
         database: 'real-time_chat',
-        // username: env('DATABASE_USERNAME', ''),
-        // password: env('DATABASE_PASSWORD', ''),
       },
-      // options: {
-      //   authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
-      //   ssl: env.bool('DATABASE_SSL', false),
-      // },
     },
   },
 });
